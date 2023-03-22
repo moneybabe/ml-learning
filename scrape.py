2e33802a8cb6a6bfb4427df04fec7182f94f8b6c
Neo Lee <neo.lky852@gmail.com>
2023-03-22 03:54:37 -0700

    learning

--- scrape.py
+++ scrape.py
@@ -3,47 +3,53 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import re
 
-url = "https://hk.investing.com/economic-calendar/cpi-733"
 
-def truncate_unwanted_date(date):
-    date = re.sub('[^0-9]','/', date)
-    indices = [x for x, v in enumerate(date) if v == '/']
-    date = date[0:indices[1]]
+class Scrape:
 
-    return date
+    def __init__(self, url):
+        self.url = url
 
-def scrape_to_csv():
-    # load webpage
-    caps = DesiredCapabilities().CHROME
-    caps["pageLoadStrategy"] = "eager"
-    driver = webdriver.Chrome(desired_capabilities=caps)
-    driver.get(url)
+    def __truncate_unwanted_date(self, date):
+        date = re.sub('[^0-9]','/', date)
+        indices = [x for x, v in enumerate(date) if v == '/']
+        date = date[0:indices[1]]
 
-    # expand the table fully
-    expand_button = driver.find_element_by_id("showMoreHistory733")
-    while True:
-        try:
-            expand_button.click()
-            time.sleep(0.05)
-        except Exception as e:
-            print(e)
-            break
+        return date
 
-    # copy the wanted data
-    table = driver.find_element_by_tag_name("tbody")
-    rows = table.find_elements_by_tag_name("tr")
-    for i in range(0, len(rows)):
-        date = rows[i].find_element_by_class_name("left").text
-        percentage = rows[i].find_element_by_class_name("noWrap").text
-        date = truncate_unwanted_date(date)
-        rows[i] = date + "," + percentage + "\n"
+    def scrape_to_csv(self):
+        # load webpage
+        caps = DesiredCapabilities().CHROME
+        caps["pageLoadStrategy"] = "eager"
+        driver = webdriver.Chrome(desired_capabilities=caps)
+        driver.get(url)
 
-    driver.quit()
+        # expand the table fully
+        expand_button = driver.find_element_by_id("showMoreHistory733")
+        while True:
+            try:
+                expand_button.click()
+                time.sleep(0.05)
+            except Exception as e:
+                print(e)
+                break
 
-    # export csv file
-    with open("data.csv", "w") as data:
-        for i in rows:
-            data.write(i)
+        # copy the wanted data
+        table = driver.find_element_by_tag_name("tbody")
+        rows = table.find_elements_by_tag_name("tr")
+        for i in range(0, len(rows)):
+            date = rows[i].find_element_by_class_name("left").text
+            percentage = rows[i].find_element_by_class_name("noWrap").text
+            date = self.__truncate_unwanted_date(date)
+            rows[i] = date + "," + percentage + "\n"
+
+        driver.quit()
+
+        # export csv file
+        with open("data.csv", "w") as data:
+            for i in rows:
+                data.write(i)
 
 if __name__ == "__main__":
-    scrape_to_csv()
+    url = "https://hk.investing.com/economic-calendar/cpi-733"
+    web = Scrape(url)
+    web.scrape_to_csv()
